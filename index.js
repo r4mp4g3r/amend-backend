395230a850e996a307579f97e4def00524676506
Ankan Mandal <72788172+ankan-mandal-1@users.noreply.github.com>
2024-08-06 18:38:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+const app = express();
+import cloudinary from "cloudinary"
+import cors from "cors"
+const PORT = process.env.PORT || 5000
+
+import userRoutes from "./routes/userRoute.js"
+import dashboardRoutes from "./routes/dashboardRoute.js"
+import connectDB from "./config/db.js";
+
+connectDB()
+
+cloudinary.config({ 
+    cloud_name: 'dfflk6oiq', 
+    api_key: '924287212334238', 
+    api_secret: 'I4JTW32UZGDX8o-JdRkp3yDmzeU' // Click 'View Credentials' below to copy your API secret
+});
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Hello")
+})
+
+app.use(express.json())
+app.use(express.static("public"))
+
+app.use("/api/v1", userRoutes)
+app.use("/api/v1/user", dashboardRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`)
+})
