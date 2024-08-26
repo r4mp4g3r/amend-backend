1f86fa5f0d00610b68a2d296d41c0411b1bed5aa
r4mp4g3r <ram.pachi.tej@gmail.com>
2024-08-26 19:25:15 +0800

    Update sendEmail.js
    updated send email issues

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,20 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
 const sendEmail = async (to, subject, text) => {
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
+            pass: process.env.EMAIL_PASS,
+        },
     });
 
     await transporter.sendMail({
         from: process.env.EMAIL_USER,
         to,
         subject,
-        text
+        text,
     });
 };
 
-module.exports = sendEmail;
+export default sendEmail;
